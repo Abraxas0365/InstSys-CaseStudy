1b75d81d2b5cf3395e77b3222c2f3302bfbf0fc1
乃ЕTA <TA>
2025-10-17 20:42:49 +0800

    added config.py

--- express-backend/src/modules/connection.js
+++ express-backend/src/modules/connection.js
@@ -1,23 +1,8 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const { userSchema, fileSchema } = require('./../components/constructor');
 const url_1 = "mongodb://localhost:27017/accounts";
 const url_2 = "mongodb://localhost:27017/file";
-
-const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  student_name: { type: String, required: true },
-  year: { type: String, required: true },
-  course: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, required: true },
-})
-const fileSchema = new mongoose.Schema({
-  file_name: { type: String, require: true },
-  fileType: { type: String, required: true },
-  file: { type: Buffer, required: true },
-  fileHash: { type: String, required: true, unique: true },
-})
 
 const AccountsConnection = mongoose.createConnection(url_1, {
   useNewUrlParser: true,
